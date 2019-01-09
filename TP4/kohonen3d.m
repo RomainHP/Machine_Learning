04b4374b5696fd002679f50baec70a5bc55471e8
Romain Charpentier <romain.charpentier10@gmail.com>
2019-01-09 17:12:56 +0100

    run + 3d

--- TP4/kohonen3d.m
+++ TP4/kohonen3d.m
@@ -14,47 +14,55 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## -*- texinfo -*- 
-## @deftypefn {} {@var{retval} =} kohonen2d (@var{input1}, @var{input2})
+## @deftypefn {} {@var{retval} =} kohonen3d (@var{input1}, @var{input2})
 ##
 ## @seealso{}
 ## @end deftypefn
 
 ## Author: Romain <romain@Romain-PC>
-## Created: 2019-01-08
+## Created: 2019-01-09
 
-function [w] = kohonen2d(x,K,mu,sigma,nbiter)
+function [w] = kohonen3d(x,K,mu,sigma,nbiter)
   N = size(x)(2);
-  w = zeros(K,K,2);
+  w = zeros(K,K,K,3);
   for i = 1:K
     for j = 1:K
-      w(i,j,1) = rand(1);
-      w(i,j,2) = rand(1);
+      for k = 1:K
+        w(i,j,k,1) = rand(1);
+        w(i,j,k,2) = rand(1);
+        w(i,j,k,3) = rand(1);
+      endfor
     endfor
   endfor
   mut = mu;
   sigmat = sigma;
   for i = 1:nbiter
     individuAuHasard = randi(N);
-    q = ones(1,2);  ## index du composant gagnant
+    q = ones(1,3);  ## index du composant gagnant
     for j = 1:K
       for k = 1:K
-        d = norm(x(:,individuAuHasard)-[w(j,k,1) w(j,k,2)]);
-        if ((j==1) && (k==1))
-          dmin = d;
-        else
-          if (d < dmin)
+        for h = 1:K
+          d = norm(x(:,individuAuHasard) - [w(j,k,h,1) ; w(j,k,h,2) ; w(j,k,h,3)]);
+          if (j==1 && k==1)
             dmin = d;
-            q = [j k];
-          endif
-        endif 
+          else
+            if (d < dmin)
+              dmin = d;
+              q = [j k h];
+            endif
+          endif 
+        endfor
       endfor
     endfor
     for j = 1:K
       for k = 1:K
-        d = norm(q-[j k]);
-        h = mut * exp(-(d*d)/(2*sigmat*sigmat));
-        w(j,k,1) = w(j,k,1) + h * (x(1,individuAuHasard) - w(j,k,1));
-        w(j,k,2) = w(j,k,2) + h * (x(2,individuAuHasard) - w(j,k,2));
+        for h = 1:K
+          d = norm(q - [j k h]);
+          h = mut * exp(-(d*d)/(2*sigmat*sigmat));
+          w(j,k,h,1) = w(j,k,h,1) + h * (x(1,individuAuHasard) - w(j,k,h,1));
+          w(j,k,h,2) = w(j,k,h,2) + h * (x(2,individuAuHasard) - w(j,k,h,2));
+          w(j,k,h,3) = w(j,k,h,3) + h * (x(2,individuAuHasard) - w(j,k,h,3));
+        endfor
       endfor
     endfor
     ## mu et sigma decroissent
---
+++ TP4/runKohonen2d.m
@@ -0,0 +1,34 @@
+## Copyright (C) 2019 Romain
+## 
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} runKohonen2d (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Romain <romain@Romain-PC>
+## Created: 2019-01-09
+
+function runKohonen2d ()
+  load data.mat;
+  x = xdisc;
+  K = 3;
+  mu = 0.5;
+  sigma = 3;
+  nbiter = 1000;
+  w = kohonen2d(x,K,mu,sigma,nbiter);
+  affiche_grille(w);
+endfunction
